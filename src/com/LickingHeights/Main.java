78f9ed1efde9df67369abe748894a5f83e0f0a87
Muna Chimoriya <muna.chimoriya.2022@student.lhschools.org>
2020-03-13 12:04:46 -0400

    Ternary Operator built for hours

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,33 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	modulusClock();
+
+    }
+    public static void modulusClock(){
+        Scanner modulusClock;
+        modulusClock = new Scanner(System.in);
+        int hours;
+        int minutes;
+        System.out.println("Enter a value for hours");
+          hours = modulusClock.nextInt();
+        System.out.println("Enter a value for minutes");
+         minutes = modulusClock.nextInt();
+
+        System.out.println(" The time is: " + hours + ":" + minutes);
+         hours = minutes >= 60 ? ++hours : hours ;
+         minutes = minutes %60;
+
+         hours = hours %12;
+         
+
+
+
+
     }
 }
+
